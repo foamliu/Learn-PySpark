1fdbf6428a243e9f8bc7451fea17d5703be9d0ee
unknown <foamliu@yeah.net>
2018-11-10 15:39:14 +0800

    update

---
+++ firstapp.py
@@ -0,0 +1,8 @@
+from pyspark import SparkContext
+logFile="file:///home/ubuntu/Downloads/spark-2.3.2-bin-hadoop2.7/README.md"
+sc=SparkContext("local","first app")
+logData=sc.textFile(logFile).cache()
+numAs=logData.filter(lambda s: 'a' in s).count()
+numBs=logData.filter(lambda s: 'b' in s).count()
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+ 
